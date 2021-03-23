4c931c27f84f9d4b9d689f3038c48fe758d8f186
Charles Ezan <charles.ezan1@uqac.ca>
2021-03-23 22:05:45 +0100

    new State new Json rules feature Agent

--- Chourbland/Properties/Agent.cs
+++ Chourbland/Properties/Agent.cs
@@ -1,13 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace chourbland
 {
 
 	public class Agent
 	{
-		public Agent()
-		{
-		}
-	}
+
+        // Percepts - liste des capteurs de l'agent
+        public List<String> percepts = new List<String>(new string[] { "smell", "wind", "shine" });
+
+        // Intentions - Liste des actions disponibles pour l'agent
+        public List<String> intentions = new List<String>(new string[] { "up", "down", "left", "right", "shoot" });
+
+        // Desire - Trouver le portail
+        public Object portal_searched = new Object();
+
+        // Podition de l'agent
+        public Tuple<int, int> pos_agent = new Tuple<int, int> (0,0);
+
+        public int performance_indicator = 0;
+
+        public void Shoot_rock(Tuple<int,int> target_pos)
+        {
+/*            if element on the cell == "monster"
+                kill_monster(target_pos);
+*/
+        }
+
+    }
 
 }
