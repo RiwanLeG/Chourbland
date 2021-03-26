0565ab51dab53b538fb6bcf4aab7caa49d77d62f
RiwanLegal <rlegbzh@gmail.com>
2021-03-26 18:42:44 -0400

    Json Rules

--- Chourbland/Agent.cs
+++ Chourbland/Agent.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.IO;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 namespace Chourbland
 {
     public class Agent
@@ -132,5 +137,25 @@ namespace Chourbland
             }
             return next_pos_agent;
         }
+
+
+        // Fonction pour lire le JSON
+
+        public void Load_Json()
+        {
+            // read JSON directly from a file
+            // @"c:\Users\riwan\Desktop\UQAC\Trimestre 1\Intelligence artificielle\Travail 3\Chourbland\Rules.json"
+            using (StreamReader file = File.OpenText(@"c:\Users\riwan\Desktop\UQAC\Trimestre 1\Intelligence artificielle\Travail 3\Chourbland\Rules.json"))
+            using (JsonTextReader reader = new JsonTextReader(file))
+            {
+                JObject o2 = (JObject)JToken.ReadFrom(reader);
+                foreach (var element in o2)
+                {
+                    Console.WriteLine("if : " + element.Key);
+                    Console.WriteLine("else : " + element.Value["danger"].ToString());
+                }
+            }
+        }
+
     }
 }
