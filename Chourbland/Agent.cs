ca7d324a535ed5f2cece0854ee87aeac25cbc3ac
RiwanLegal <rlegbzh@gmail.com>
2021-03-26 18:29:36 -0400

    Version avec recherche dans le Json

--- Chourbland/Agent.cs
+++ Chourbland/Agent.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace chourbland
+using System.IO;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Chourbland
 {
-
-	public class Agent
-	{
+    public class Agent
+    {
 
         // Percepts - liste des capteurs de l'agent
         public List<String> percepts = new List<String>(new string[] { "smell", "wind", "shine" });
@@ -19,17 +24,33 @@ namespace chourbland
         public Object portal_searched = new Object();
 
         // Podition de l'agent
-        public Tuple<int, int> pos_agent = new Tuple<int, int> (0,0);
+        public Tuple<int, int> pos_agent = new Tuple<int, int>(0, 0);
 
         public int performance_indicator = 0;
 
-        public void Shoot_rock(Tuple<int,int> target_pos)
+        public void Shoot_rock(Tuple<int, int> target_pos)
         {
-/*            if element on the cell == "monster"
-                kill_monster(target_pos);
-*/
+            /*            if element on the cell == "monster"
+                            kill_monster(target_pos);
+            */
         }
 
-    }
+        // Fonction pour lire le JSON
 
+        public void Load_Json()
+        {
+            // read JSON directly from a file
+            // @"c:\Users\riwan\Desktop\UQAC\Trimestre 1\Intelligence artificielle\Travail 3\Chourbland\Rules.json"
+            using (StreamReader file = File.OpenText(@"c:\Users\riwan\Desktop\UQAC\Trimestre 1\Intelligence artificielle\Travail 3\Chourbland\Rules.json"))
+            using (JsonTextReader reader = new JsonTextReader(file))
+            {
+                JObject o2 = (JObject)JToken.ReadFrom(reader);
+                foreach(var element in o2)
+                {
+                    Console.WriteLine("if : " + element.Key);
+                    Console.WriteLine("else : " + element.Value["danger"].ToString());
+                }
+            }
+        }
+    }
 }
